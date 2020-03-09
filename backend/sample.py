8f0bc5a8d70e438560551fd4782ed1dcea41cf2a
gilesccs <54528035+gilesccs@users.noreply.github.com>
2020-03-09 15:25:28 +0800

    Update sample.py

--- backend/sample.py
+++ backend/sample.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 
-
+name = 'testing'
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/book'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
