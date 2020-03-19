f096dd13e0776c455b146567aa855fa8e9b8f3ab
gilesccs <54528035+gilesccs@users.noreply.github.com>
2020-03-19 12:28:04 +0800

    Finished image ms

--- backend/image.py
+++ backend/image.py
@@ -3,6 +3,8 @@ from flask import request
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask import Response
+import base64
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/sglovelah_image'
@@ -45,5 +47,15 @@ def retrieve():
     ##pass profileID to this function
     ##hardcode for now
     profileID = 2
+    userprofile = image.query.filter_by(profileID=profileID).first()
+    filepic = userprofile.data
+    # encoded = base64.b64encode(filepic)
+    # return encoded
+    ## img src = this response.
+    return Response(filepic, mimetype='image/jpeg')
+    
+
+
+
 if __name__ == "__main__":
     app.run(port=5000,debug=True)
