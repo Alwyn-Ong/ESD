4777cda4a3ada3730e50c130dc8284134c4aee4b
Alwyn-Ong <52643926+Alwyn-Ong@users.noreply.github.com>
2020-04-06 18:08:11 +0800

    Revert "Add batch files to automate opening of microservices"
    This reverts commit 0281f16be484d63753609b4d21f3273aa9f924f8.

--- start_microservices.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start python backend/profile.py
-start python backend/image.py
-start python backend/recommendation.py
-start python backend/match_receiver.py
-start python backend/match.py
-start python backend/chatms/chat.py
