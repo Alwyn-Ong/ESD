71b62189d1d4941684082da7ece7d1a9c5451f82
kinhou <47879876+kinhou@users.noreply.github.com>
2020-04-06 18:07:29 +0800

    Final Copy

---
+++ start_microservices.bat
@@ -0,0 +1,13 @@
+@echo off
+start python backend/profile.py
+start python backend/image.py
+start python backend/recommendation.py
+start python backend/match_receiver.py
+start python backend/match.py
+
+start python backend/chatms/chat.py
+start python backend/chatms/chatsocketone.py
+start python backend/chatms/chatsockettwo.py
+start python backend/chatms/chatsocketthree.py
+start python backend/chatms/chatsocketfour.py
+start python backend/chatms/chatsocketfive.py
